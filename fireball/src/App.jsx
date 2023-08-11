1df52967710055d0c742cc6f5d5deeaf35058040
emmacodes <binma.emma@gmail.com>
2023-08-10 17:02:23 -0700

    set up

---
+++ fireball/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-2xl text-indigo-700'>Hello World</div>
+    </>
+  )
+}
+
+export default App
