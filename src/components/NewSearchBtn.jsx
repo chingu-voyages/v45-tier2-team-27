b3cedd9c47d3a156efb2ea4a86353b1ea72893e4
AOA19 <aakinyemi19@gmail.com>
2023-08-24 16:02:05 -0400

    Created search results table, and created new search button component

---
+++ src/components/NewSearchBtn.jsx
@@ -0,0 +1,14 @@
+export default function NewSearchBtn() {
+  return (
+    <div className="mx-10 my-12 py-1 relative">
+      <img
+        src="/images/large-black-border.png"
+        alt=""
+        className="large-black-border absolute -top-2 left-0 right-0 m-auto"
+      />
+      <button type="button" className="uppercase relative">
+        Start new search
+      </button>
+    </div>
+  );
+}
