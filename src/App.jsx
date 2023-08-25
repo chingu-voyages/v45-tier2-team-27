e054272d411d64ab015cf8c3c4c1f705d059a8da
David Riley <Dril112191@gmail.com>
2023-08-25 12:13:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { createContext, useState } from "react";
 import './App.css';
-import './landing.css';
-import './connectingLines.css';
-import './trapezoid.css';
-import './borderImages.css'
+import '/src/landing.css';
+import '/src/connectingLines.css';
+import '/src/trapezoid.css';
+import '/src/borderImages.css'
 import Landing from './components/Landing';
 import { Route, Routes } from 'react-router-dom';
 import LineChart from './components/LineChart';
