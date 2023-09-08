c052e709651e582ee5b117902056f493dba80827
David Riley <Dril112191@gmail.com>
2023-09-07 20:41:33 -0400

    Added the users selected meteorite data above the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,43 +9,44 @@ export default function Map() {
 
   return (
     <>
-    <h1 className='text-2xl border'>
-      Geolocation Coordinates
-    </h1>
-<MapContainer
-  className="full-height-map"
-  center={[selectedMeteorite.geolocation.latitude, selectedMeteorite.geolocation.longitude]}
-  zoom={3}
-  scrollWheelZoom={true}
-  >
-  <TileLayer
-    url={`${darkMode ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" : 'https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png'}`}
-  />
-
-      {/* Display a marker and popup when a meteorite is selected */}
-      {selectedMeteorite && (
-        <Marker
-          position={[
-            selectedMeteorite.geolocation.latitude,
-            selectedMeteorite.geolocation.longitude,
-          ]}
-          icon={new Icon({
-            iconUrl: "./images/target.png", // Replace with the path to your custom marker icon
-            iconSize: [20, 20], // Set the size of your custom marker icon
-            
-          })}
+      <tr className='map-selected-container'>
+        <td className="selected-map-data">{selectedMeteorite.name.toString()}</td>
+        <td className="selected-map-data">{selectedMeteorite.year.toString()}</td>
+        <td className="selected-map-data">{selectedMeteorite.recclass.toString()}</td>
+        <td className="selected-map-data">{selectedMeteorite.mass.toString()}</td> 
+      </tr>
+       
+      <MapContainer
+        className="full-height-map"
+        center={[selectedMeteorite.geolocation.latitude, selectedMeteorite.geolocation.longitude]}
+        zoom={3}
+        scrollWheelZoom={true}
         >
-          <Popup>
-            Meteorite Location: {selectedMeteorite.name}
-          <br />
-            Latitude: {selectedMeteorite.geolocation.latitude}
-          <br />
-            Longitude: {selectedMeteorite.geolocation.longitude}
-          </Popup>
-        </Marker>
-      )}
-    </MapContainer>
+        <TileLayer
+          url={`${darkMode ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" : 'https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png'}`}
+        />
+
+        {selectedMeteorite && (
+          <Marker
+            position={[
+              selectedMeteorite.geolocation.latitude,
+              selectedMeteorite.geolocation.longitude,
+            ]}
+            icon={new Icon({
+              iconUrl: "./images/target.png", 
+              iconSize: [20, 20],
+            })}
+          >
+            <Popup>
+              Meteorite Location: {selectedMeteorite.name}
+            <br />
+              Latitude: {selectedMeteorite.geolocation.latitude}
+            <br />
+              Longitude: {selectedMeteorite.geolocation.longitude}
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
     </>
   );
-  
 }
